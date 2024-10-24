534f508d584e206a05f4b2bbbde784188f99fc5b
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-10-24 22:52:01 +0700

    coding tipis

---
+++ example/hello.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_packages/hello.dart';
+import 'package:belajar_dart_packages/src/cutemer.dart'; // -Irfan note : jangan ngabil bagian src sangat tidak direkomendasikan lebih baik lasung slas nama file yang ingin di gunakan
+
+void main() {
+  print(sayHeloo("irfan"));
+
+  var custemer = Custemer();
+}
--- lib/hello.dart
+++ lib/hello.dart
@@ -1,4 +1,7 @@
-library belajar_dart_packages;
+library belajar_dart_packages; //- Irfan nama libary harus disamakan dengan project yang kita buat janhan sampai kosong karana akan membuat bentrok
+//Kata kunci library di Dart digunakan untuk mendefinisikan dan mengelompokkan kode dalam unit yang terpisah, memungkinkan pengembang untuk mengorganisir kode secara modular. Dengan menggunakan library, Anda dapat memisahkan fungsi, kelas, dan variabel ke dalam file yang berbeda, sehingga memudahkan pengelolaan dan pemeliharaan kode.
 
-export 'src/say_hello.dart';
+export 'src/say_hello.dart'; // -Irfan kata kunci export di gunakan  untu membakikan  simbol dari satu libary ke libary lain
+//Dengan menggunakan export, Anda dapat membuat simbol dari library tertentu tersedia untuk digunakan di library lain tanpa perlu mengimpor setiap simbol secara individual.
 export "src/cutemer.dart" show Produk;
+// Kata kunci show digunakan bersamaan dengan export untuk membatasi simbol yang diekspor. Dengan menggunakan show, Anda hanya dapat mengekspor simbol tertentu dari library, sehingga menghindari potensi konflik nama atau kebingungan.
